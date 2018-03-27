b9c990df0b4df5e7bfbe5995e2508c2314d8b708
Richard Kim <richeekim@gmail.com>
2018-03-27 23:19:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -43,8 +43,4 @@ def for_iterator(number_of_times)
   for counter in phrasecount
     puts phrase
   end
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-
-
 end
